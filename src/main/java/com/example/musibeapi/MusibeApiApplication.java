435af5f8a9e41d4849f06c8b6aeba7b684b46a63
kye8524 <cjy7686@naver.com>
2023-04-05 18:24:19 +0900

    create SpringBoot project

---
+++ src/main/java/com/example/musibeapi/MusibeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.musibeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusibeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusibeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musibeapi/MusibeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musibeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusibeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
